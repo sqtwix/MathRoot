e2cfdfbd30059172d8d9d030f37474792ed91eb4
sqtwix <ivan20140767@gmail.com>
2026-02-21 21:57:53 +0300

    feat: inputs of values in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,53 @@
 package org.example;
 
-import java.util.*;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("=== РЕШЕНИЕ УРАВНЕНИЙ МЕТОДОМ БИСЕКЦИИ ===");
 
-        BisectionMethod.printRoot("x^2 - 4", -3, 0, 0.0001);
+        System.out.println("Введите уравнение (по типу x^2 + x - 3):");
+        String equation = sc.nextLine();
+
+        double a = 0.0, b = 0.0, epsilon = 0.0;
+        boolean inputValid = true;
+
+        try {
+            System.out.println("Введите a:");
+            a = sc.nextDouble();
+        } catch (Exception ex) {
+            System.out.println("Ошибка при вводе a: " + ex.getMessage());
+            inputValid = false;
+        }
+
+        if (inputValid) {
+            try {
+                System.out.println("Введите b:");
+                b = sc.nextDouble();
+            } catch (Exception ex) {
+                System.out.println("Ошибка при вводе b: " + ex.getMessage());
+                inputValid = false;
+            }
+        }
+
+        if (inputValid) {
+            try {
+                System.out.println("Введите epsilon:");
+                epsilon = sc.nextDouble();
+            } catch (Exception ex) {
+                System.out.println("Ошибка при вводе epsilon: " + ex.getMessage());
+                inputValid = false;
+            }
+        }
+
+        if (inputValid) {
+            BisectionMethod.printRoot(equation, a, b, epsilon);
+        } else {
+            System.out.println("Программа завершена из-за ошибок ввода.");
+        }
+
+        sc.close();
     }
 }
